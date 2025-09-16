5ade1ecfbe4b9a35a094b699b53216640c3889de
iruazu <ygnk0805@outlook.jp>
2025-09-16 10:26:01 +0900

    "Initial commit: setup Python and C++ practice"

---
+++ cpp/test.cpp
@@ -0,0 +1,11 @@
+// test.cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "You entered: " << n << endl;
+    return 0;
+}
---
+++ python/test.py
@@ -0,0 +1,3 @@
+# test.py
+n = int(input("Enter a number: "))
+print(f"You entered: {n}")
